9fda9167841ace14d581928afb880f3282a7abb1
chrisbooth <chris.booth.ct@gmail.com>
2023-10-02 16:12:33 +0200

    debugging

--- src/trello/trello.module.ts
+++ src/trello/trello.module.ts
@@ -1,8 +1,11 @@
+// src/trello/trello.module.ts
+
 import { Module } from '@nestjs/common';
+import { TrelloController } from './trello.controller';
 import { TrelloService } from './trello.service';
 
 @Module({
+  controllers: [TrelloController],
   providers: [TrelloService],
-  exports: [TrelloService],
 })
 export class TrelloModule {}
