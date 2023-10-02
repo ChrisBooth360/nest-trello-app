9f8261577ce773480fbb713f878c8d0f89f12b55
chrisbooth <chris.booth.ct@gmail.com>
2023-10-02 16:12:33 +0200

    debugging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
+// src/app.module.ts
+
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config'; // Import ConfigModule
-import { TrelloModule } from './trello/trello.module';
+import { ConfigModule } from '@nestjs/config';
+import { TrelloModule } from './trello/trello.module'; // Import the TrelloModule
 
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal: true}), // Configure ConfigModule
     TrelloModule,
-  ],
+  ]
 })
 export class AppModule {}
+
--- src/trello/trello.service.ts
+++ src/trello/trello.service.ts
@@ -1,25 +1,38 @@
+// src/trello/trello.service.ts
+
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
-import { ConfigService } from '@nestjs/config'; // Import ConfigService
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class TrelloService {
   private readonly apiKey: string;
   private readonly apiToken: string;
+  private readonly boardId: string;
 
   constructor(private readonly configService: ConfigService) {
     // Retrieve Trello API credentials from environment variables using ConfigService.
     this.apiKey = this.configService.get<string>('TRELLO_API_KEY');
     this.apiToken = this.configService.get<string>('TRELLO_API_TOKEN');
+    this.boardId = 'GU6JzE3p'
   }
 
-  async getBoardTasks(boardId: string) {
-    const url = `https://api.trello.com/1/boards/${boardId}/cards?key=${this.apiKey}&token=${this.apiToken}`;
+  async getTasksFromBoard(): Promise<any> {
     try {
-      const response = await axios.get(url);
+      const response = await axios.get(
+        `https://api.trello.com/1/boards/${this.boardId}/cards`,
+        {
+          params: {
+            key: this.apiKey,
+            token: this.apiToken,
+          },
+        }
+      );
+
       return response.data;
     } catch (error) {
-      throw new Error('Unable to fetch board tasks from Trello API');
+      throw new Error('Error fetching tasks from Trello: ' + error.message);
     }
   }
 }
+
