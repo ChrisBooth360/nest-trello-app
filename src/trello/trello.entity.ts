c40ed95f03e60cc4affbd875f901331d0dab3ac7
chrisbooth <chris.booth.ct@gmail.com>
2023-10-03 15:10:12 +0200

    Added columns and more detailed info for SQLite DB

--- src/trello/trello.entity.ts
+++ src/trello/trello.entity.ts
@@ -7,13 +7,16 @@ export class TrelloEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ name: 'name' }) // Map 'name' property to 'name' column in the database
   name: string;
 
-  @Column({ nullable: true })
-  list: string;
+  @Column({ name: 'completed', nullable: true }) // Map 'completed' property to 'completed' column in the database
+  completed: boolean;
 
-  @Column({ nullable: true })
+  @Column({ name: 'dueDate', nullable: true }) // Map 'dueDate' property to 'dueDate' column in the database
   dueDate: string;
+
+  @Column({ name: 'cardId', nullable: true }) // Map 'cardId' property to 'cardId' column in the database
+  cardId: string;
 
 }
