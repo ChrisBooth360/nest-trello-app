9b59b4f41b988d773689068cc7333434fd00b386
chrisbooth <chris.booth.ct@gmail.com>
2023-10-04 13:30:26 +0200

    Fixed HTML and JS to pull Trello tasks and display them. Added unit tests for TrelloService

--- src/trello/trello.service.spec.ts
+++ src/trello/trello.service.spec.ts
@@ -1,48 +1,105 @@
-// src/trello/trello.service.spec.ts
+// trello.service.spec.ts
 
 import { Test, TestingModule } from '@nestjs/testing';
 import { TrelloService } from './trello.service';
-import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm';
-import { TrelloEntity } from './trello.entity';
 import { ConfigService } from '@nestjs/config';
-import { Repository } from 'typeorm'; // Import Repository
+import { getRepositoryToken } from '@nestjs/typeorm';
+import axios from 'axios';
+import { TrelloEntity } from './trello.entity';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+jest.mock('axios');
 
 describe('TrelloService', () => {
-  let service: TrelloService;
+  let trelloService: TrelloService;
+  let axiosGetMock: jest.Mock;
 
-  beforeAll(async () => {
+  const mockConfigService = {
+    get: jest.fn((key) => {
+      if (key === 'TRELLO_API_KEY') {
+        return process.env.TRELLO_API_KEY;
+      } else if (key === 'TRELLO_API_TOKEN') {
+        return process.env.TRELLO_API_TOKEN;
+      }
+      return undefined; // Handle other config keys as needed
+    }),
+  };
+
+  const mockTaskRepository = {
+    findOne: jest.fn(),
+    save: jest.fn(),
+    find: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    axiosGetMock = jest.fn();
+    (axios.get as jest.Mock) = axiosGetMock;
+
     const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        TypeOrmModule.forRoot({
-          type: 'sqlite',
-          database: ':memory:', // Use an in-memory database for testing
-          entities: [TrelloEntity],
-          synchronize: true,
-        }),
-        TypeOrmModule.forFeature([TrelloEntity]),
-      ],
       providers: [
         TrelloService,
         {
           provide: ConfigService,
-          useValue: {
-            get: () => 'your-config-values',
-          },
+          useValue: mockConfigService,
         },
         {
-          provide: getRepositoryToken(TrelloEntity), // Provide TrelloEntityRepository
-          useClass: Repository, // Remove this line
+          provide: getRepositoryToken(TrelloEntity),
+          useValue: mockTaskRepository,
         },
       ],
     }).compile();
 
-    service = module.get<TrelloService>(TrelloService);
+    trelloService = module.get<TrelloService>(TrelloService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
-  // Write your test cases here
+  describe('getBoardTasks', () => {
+    it('should fetch board tasks from Trello', async () => {
+      const boardId = 'sample-board-id';
+      const responseData = [
+        {
+          id: 'task-id-1',
+          name: 'Task 1',
+          due: null,
+          desc: null,
+        },
+        // Add more task data as needed
+      ];
+
+      axiosGetMock.mockResolvedValueOnce({ data: responseData });
+
+      const result = await trelloService.getBoardTasks(boardId);
+
+      expect(axiosGetMock).toHaveBeenCalledWith(
+        `https://api.trello.com/1/boards/${boardId}/cards`,
+        {
+          params: {
+            key: expect.any(String),
+            token: expect.any(String),
+          },
+        }
+      );
+
+      expect(result.success).toBeTruthy();
+      expect(result.data).toEqual(responseData);
+    });
+
+    it('should handle errors when fetching tasks from Trello', async () => {
+      const boardId = 'sample-board-id';
+
+      axiosGetMock.mockRejectedValueOnce(new Error('Network Error'));
+
+      const result = await trelloService.getBoardTasks(boardId);
+
+      expect(result.success).toBeFalsy();
+      expect(result.error).toContain('Error fetching tasks from Trello');
+    });
+  });
+
+  // Add more test cases for other methods as needed
 
 });
